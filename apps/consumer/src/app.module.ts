ee3a956d5ff58d3611fcd36033fc46311c291d7f
Masz <mawin38408@gmail.com>
2022-10-12 12:02:10 +0700

    fix .env

--- apps/consumer/src/app.module.ts
+++ apps/consumer/src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot({
-    envFilePath: '.env',
+    envFilePath: 'apps/consumer/.env',
   })],
   controllers: [AppController],
   providers: [AppService],
