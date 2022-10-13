22533c83f97bb609a7b0c7f8fec13e62ea1ba298
Masz <mawin38408@gmail.com>
2022-10-13 17:54:12 +0000

    add dev container config

--- apps/producer/src/config/configuration.ts
+++ apps/producer/src/config/configuration.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config';
 interface Config {
   rabbitmq: string;
 }
+
 const envConfigObject = registerAs<Config>('config', () => ({
   rabbitmq: process.env.RABBITMQ_URL,
 }));
